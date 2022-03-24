0366405937d19649318fce78f3d3a17386095a6b
komalteke <62090882+komalteke@users.noreply.github.com>
2022-03-24 21:09:22 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,27 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/*Write a program and ask the user to enter a number. The number should be between 1 to 10.
- * If the user enters a valid number, display "Valid" on the console. 
- * Otherwise, display "Invalid".
- * (This logic is used a lot in applications where values entered into input boxes need to be validated.)*/
 
-namespace first_code
+/*Write a program which takes two numbers from the console and displays the maximum of the two.*/
+
+namespace second_code
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number : ");
-            var num=Convert.ToInt32(Console.ReadLine());
-            if(num>=1 && num<=10)
+            Console.WriteLine("Enter the first number : ");
+            var num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number : ");
+            var num2=Convert.ToInt32(Console.ReadLine());
+            if(num1>num2)
             {
-                Console.WriteLine("The number entered is valid ");
+                Console.WriteLine("{0} is greater than {1}",num1,num2);
+            }
+            else if(num2 > num1)
+            {
+                Console.WriteLine("{0} is greater than {1}",num2,num1);
             }
             else
-            {
-                Console.WriteLine("invalid number");
-            }
+                Console.WriteLine("{0} and {1} are equal",num1,num2);
         }
     }
 }
