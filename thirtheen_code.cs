a0992b921ed7ee8395ee71e4c37d07914b9256a7
komalteke <62090882+komalteke@users.noreply.github.com>
2022-03-28 11:25:38 +0530

    Add files via upload

---
+++ thirtheen_code.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Write a program and ask the user to continuously enter a number or type "Quit" to exit.
+ * The list of numbers may include duplicates. 
+ * Display the unique numbers that the user has entered*/
+
+namespace thirtheen_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var l=new List<int>();
+            var temp = 0;
+            do
+            {
+                Console.WriteLine("Enter the number or Quit ");
+                var n=Console.ReadLine();
+                if (n.Equals("Quit"))
+                    break;
+                temp=Convert.ToInt32(n);
+                l.Add(temp);
+            }while(true);
+            var new_l=new List<int>();
+            foreach(int i in l)
+            {
+                if (new_l.Contains(i))
+                    continue;
+                new_l.Add(i);
+            }
+            foreach(int i in new_l)
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ twelth_code.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Write a program and ask the user to enter 5 numbers. 
+ * If a number has been previously entered, display an error message and ask the user to re-try.
+ * Once the user successfully enters 5 unique numbers, sort them and display the result on the console*/
+
+namespace twelth_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums=new List<int>();
+            do
+            {
+                Console.WriteLine("Enter the number");
+                var temp = Convert.ToInt32(Console.ReadLine());
+                if (nums.Contains(temp))
+                {
+                    Console.WriteLine("Re-enter again ");
+                    temp = Convert.ToInt32(Console.ReadLine());
+                }
+                nums.Add(temp);
+
+            } while (nums.Count!=5);
+            nums.Sort();
+            foreach(int i in nums)
+                Console.WriteLine(i);
+        }
+    }
+}
